28d9c6018575963cebf74d3cf25fe4297cc69423
lazybear0340 <awsVPNhost_lazybear0340@proton.me>
2026-07-13 11:29:08 +0200

    Delete _static/_tokgen directory

--- _static/_tokgen/example.lua
+++
@@ -1,2 +0,0 @@
-local key = "t4thxthbx4etxe4dtxe4dtxe4dtge4xdg4"
-print(key, lzysdk_tokgen(64, 3))
--- _static/_tokgen/main.lua
+++
@@ -1,6 +0,0 @@
-lzysdk_static_tokgen(length, 1/2/3)
-
-length = length of the token
-1 = letters only
-2 = numbers only
-3 = letters & numbers
