e1d21ef7c2afbc9b9caaa918936f7a9adb5222a2
lazybear0340 <awsVPNhost_lazybear0340@proton.me>
2026-07-16 13:29:22 +0200

    Update main.lua

--- .misc/.enhanced/main.lua
+++ .misc/.enhanced/main.lua
@@ -1,3 +1,10 @@
 lzysdk.misc.enhanced(start)
 lzysdk.misc.enhanced(end)
 
+--[[
+start; begins the enhanced block
+end; ends the enhanced block
+Any code between enhanced(start) and enhanced(end) will be near to impossible to recover
+
+lzysdk.misc.enhanced IS PERFORMANCE HEAVY and WILL take ALOT of storage
+]]--
