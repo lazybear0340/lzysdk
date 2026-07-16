bd605bc825c06e19656fb476a80f00823ce05ac4
lazybear0340 <awsVPNhost_lazybear0340@proton.me>
2026-07-16 14:44:42 +0200

    Update example.lua

--- .misc/.enhanced/example.lua
+++ .misc/.enhanced/example.lua
@@ -1,8 +1,8 @@
 local x = 10
-lzysdk.misc.enhanced(1)
+lzysdk.misc.enhanced(start)
   x = x * 2
   if x > 15 then
     x = x + 5
   end
-lzysdk.misc.enhanced(2)
+lzysdk.misc.enhanced(end)
 local y = x / 5
